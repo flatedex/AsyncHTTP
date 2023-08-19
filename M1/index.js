68bcffca0acd70672b2e3822536e73eead7ba9ef
flatedex <flatedex@mail.ru>
2023-08-19 13:38:08 +0300

    many improvements, asynchronous version not working yet

--- M1/index.js
+++ M1/index.js
@@ -9,55 +9,48 @@ const HOST = "0.0.0.0";
 
 const app = express();
 
-app.post('/', (req, res) => {
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.post('/', async (req, res) => {
     let name = req.body.Name;
     let message = req.body.Message;
 
-    amqp.connect('amqp://guest:guest@rabbitmq:5672/', function(error, connection) {
-        if(error) {
-            throw error;
-        }
-        connection.createChannel(function(error1, channel){
-            if(error1){
-                throw error;
-            }
-            let queue = "toM2";
-            let msg = message;
+    const connection = await amqp.connect('amqp://guest:guest@rabbitmq:5672');
+    const channel = await connection.createChannel();
+    console.log('M1 is connected to RabbitMQ');    
+    
+    let queue = "toM2";
+    let msg = message;
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
-
-            channel.sendToQueue(queue, Buffer.from(msg));
-            console.log(`Message by ${name} sent to M1`);
-        });
+    channel.assertQueue(queue, {
+           durable: false
     });
+
+    channel.sendToQueue(queue, Buffer.from(msg));
+    console.log(`Message by ${name} sent to M1`);
 });
 
 (async() =>{
-    amqp.connect('amqp://guest:guest@rabbitmq:5672/', await function(error, connection) {
-        if(error) {
-            throw error;
-        }
-        connection.createChannel(function(error1, channel){
-            if(error1){
-                throw error;
-            }
-            let queue = "toM1";
+    const connection = await amqp.connect('amqp://guest:guest@rabbitmq:5672/');
 
-            channel.assertQueue(queue, {
-                durable: false
-            });
+    const channel = await connection.createChannel();
 
-            channel.consume(queue, function(msg) {
-                console.log("Received message from M2: %s", msg.content.toString());
-            }, {
-                noAck: true
-            });
-        });
+    let queue = "toM1";
+
+    channel.consume(queue, function(msg) {
+        console.log("Received message from M2: %s", msg.content.toString());
+    }, {
+        noAck: true
     });
 });
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, HOST, async () => {
     console.log(`Service running on http://${HOST}:${PORT}`);
 });
--- M2/index.js
+++ M2/index.js
@@ -1,39 +1,37 @@
 let amqp = require('amqplib/callback_api');
 
+(async () => {
+    const connection = await amqp.connect('amqp://guest:guest@rabbitmq:5672');
+    console.log('M2 is connected to RabbitMQ');
 
-amqp.connect('amqp://guest:guest@rabbitmq:5672/', function(error, connection) {
-    if (error) {
-        throw error;
-    }
-    connection.createChannel(function(error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+    const channel = await connection.createChannel();
 
-        let queue = "ToM2";
+    let queue = "ToM2";
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        console.log("M2 is waiting for messages in %s. To exit press CTRL+C", queue);
-
-        channel.consume(queue, function(msg) {
-            console.log("Received message from M1: %s", msg.content.toString());
-        }, {
-            noAck: true
-        });
-
-        queue = "ToM1";
-
-        channel.assertQueue(queue, {
-            durable: false
-        });
-
-        newMsg = msg.content.toString().toUpperCase(); // work here is just make message uppercase
-
-        channel.sendToQueue(queue, newMsg);
-
-        console.log(`Message "${newMsg}" to M1 sent`);
+    await channel.assertQueue(queue, {
+        durable: false
     });
-});
+
+    console.log("M2 is waiting for messages in %s. To exit press CTRL+C", queue);
+
+    let newMsg = "";
+
+    await channel.consume(queue, function(msg) {
+        console.log("Received message from M1: %s", msg.content.toString());
+        newMsg = msg.content.toString().toUpperCase();
+    }, {
+        noAck: true
+    });
+
+    queue = "ToM1";
+
+    await channel.assertQueue(queue, {
+        durable: false
+    });
+
+     // work here is just make message uppercase
+
+    await channel.sendToQueue(queue, newMsg);
+
+    console.log(`Message "${newMsg}" to M1 sent`);
+})();
